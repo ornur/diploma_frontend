a32980765da1996b6c48f9b4da05d6850d643cab
ornur <nurdauletaidaruli@gmail.com>
2025-05-14 20:32:55 +0500

    feat: implement generateData function for dynamic sensor data generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,51 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export const generateData = (
+  start: Date,
+  end: Date,
+  selectedSensor: string
+): { [key: string]: number | string }[] => {
+  const data = [];
+  const timeRange = end.getTime() - start.getTime();
+  const dataPoints = 30;
+  const interval = timeRange / (dataPoints - 1);
+
+  for (let i = 0; i < dataPoints; i++) {
+    const timestamp = new Date(start.getTime() + i * interval);
+    const dataPoint: any = { timestamp };
+
+    let baseValue = 0;
+    switch (selectedSensor) {
+      case "temp":
+        baseValue = 22 + 3 * Math.sin(i / 5);
+        break;
+      case "humidity":
+        baseValue = 45 + 15 * Math.sin(i / 8);
+        break;
+      case "pressure":
+        baseValue = 1013 + 5 * Math.sin(i / 10);
+        break;
+      case "co2":
+        baseValue = 400 + 100 * Math.sin(i / 6);
+        break;
+      case "voc":
+        baseValue = 100 + 50 * Math.sin(i / 4);
+        break;
+    }
+
+    // Add real and predicted values with some variation
+    dataPoint[`${selectedSensor}_real`] = Math.round(
+      baseValue + (Math.random() - 0.5) * 5
+    );
+    dataPoint[`${selectedSensor}_predicted`] = Math.round(
+      baseValue + (Math.random() - 0.5) * 10
+    );
+    data.push(dataPoint);
+  }
+
+  return data;
+};
