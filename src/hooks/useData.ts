e8572e867188d75b77cec36ebede02165d8a5dbd
Abzal Orazbek <abzalorazbek2003@gmail.com>
2025-05-18 17:43:45 +0500

    set defaults

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { subHours } from "date-fns/subHours";
 import { useQuery } from "@tanstack/react-query";
-import { generateData2 } from "@/lib/utils";
-// import axiosInstance from "@/lib/axios";
+import axiosInstance from "@/lib/axios";
 
 export type GetData = {
   sensorName: string;
@@ -16,16 +15,22 @@ export type GetData = {
 
 export function useData() {
   const now = new Date();
-  const [selectedSensor, setSelectedSensor] = useState<string>("ap");
+  const [selectedSensor, setSelectedSensor] = useState<string>("ActivePower");
   const [date, setDate] = useState<{ start: Date; end: Date }>({
-    start: subHours(now, 1),
-    end: now,
+    start: new Date(2025, 0, 25, 8, 1, 0),
+    end: new Date(2025, 0, 25, 9, 0, 0),
   });
 
   const modelData = useQuery({
     queryKey: ["data", date.start, date.end, selectedSensor],
-    queryFn: () => generateData2(date.start, date.end, selectedSensor),
-    // queryFn: () => axiosInstance.get<GetData>("/api/data"),
+    //queryFn: () => generateData2(date.start, date.end, selectedSensor),
+    queryFn: () => axiosInstance.get<GetData>(`/api/v1/timeseries/${selectedSensor}`, {
+      params: {
+        start_date: date.start.toISOString(),
+        end_date: date.end.toISOString(),
+      }
+    }),
+    select: data => data.data 
   });
   return { modelData, selectedSensor, setSelectedSensor, date, setDate, now };
 }
