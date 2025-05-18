6d003c49835a5ddbd6b5b38230acd5da472aa549
ornur <nurdauletaidaruli@gmail.com>
2025-05-19 01:05:53 +0500

    refactor: restructure useData hook to separate API and generated data queries

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { use, useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "@/lib/axios";
+import { generateData2 } from "@/lib/utils";
 
 export type GetData = {
   sensorName: string;
@@ -19,16 +20,40 @@ export function useData() {
     end: new Date("2025-02-17T02:00:00Z"),
   });
 
-  const modelData = useQuery({
-    queryKey: ["data", date.start, date.end, selectedSensor],
-    //queryFn: () => generateData2(date.start, date.end, selectedSensor),
-    queryFn: () => axiosInstance.get<GetData>(`/api/v1/timeseries/${selectedSensor}`, {
-      params: {
-        start_date: date.start.toISOString(),
-        end_date: date.end.toISOString(),
-      }
-    }),
-    select: data => data.data 
+  const apiData = useQuery({
+    queryKey: ["api-data", date.start, date.end, selectedSensor],
+    queryFn: async () => {
+      const response = await axiosInstance.get<GetData>(`/api/v1/timeseries/${selectedSensor}`, {
+        params: {
+          start_date: date.start.toISOString(),
+          end_date: date.end.toISOString(),
+        }
+      });
+      return response.data;
+    },
+    select: data => data,
+    retry: 3,
   });
-  return { modelData, selectedSensor, setSelectedSensor, date, setDate};
+
+  const generatedData = useQuery({
+    queryKey: ["generated-data", date.start, date.end, selectedSensor],
+    queryFn: () => generateData2(date.start, date.end, selectedSensor),
+    enabled: apiData.isError,
+  });
+
+  const modelData = useMemo(() => {
+    if (apiData.isError) {
+      console.log("API data error, using generated data", apiData.error);
+      return generatedData;
+    }
+    return apiData;
+  }, [apiData, generatedData]);
+
+  return { 
+    modelData, 
+    selectedSensor, 
+    setSelectedSensor, 
+    date, 
+    setDate,
+  };
 }
